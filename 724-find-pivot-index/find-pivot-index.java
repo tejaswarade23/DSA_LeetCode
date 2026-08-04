9c185ee9264d63144eda752abee7230560fdf56c
Tejas Warade <tejaswarade23@gmail.com>
2026-08-05 00:21:26 +0530

    Time: 1 ms (97.45%) | Memory: 47.2 MB (78.89%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i];
+        }
+        int leftSum = 0;
+        for(int i = 0; i < nums.length; i++){
+
+            int rightSum = sum - nums[i] - leftSum;
+
+            if(leftSum == rightSum){
+                return i;
+            }
+           leftSum += nums[i];
+        }
+        return -1;
+    }
+}
