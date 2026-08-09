2049ca4a280fe1d08c534ad02afb2c43f8ae9408
Tejas Warade <tejaswarade23@gmail.com>
2026-08-09 23:30:02 +0530

    Time: 24 ms (53.29%) | Memory: 65.5 MB (42.85%) - LeetSync

---
+++ 525-contiguous-array/contiguous-array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findMaxLength(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+
+        int prefixSum = 0;
+        int maxLen = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            prefixSum += (nums[i] == 1) ? 1 : -1;
+
+            if(map.containsKey(prefixSum)){
+               maxLen = Math.max(maxLen, i - map.get(prefixSum));
+            }else{
+                map.put(prefixSum, i);
+            }
+        }
+        return maxLen;
+    }
+}
